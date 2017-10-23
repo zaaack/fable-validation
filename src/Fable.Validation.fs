517e8f5e7297241c65397a32b4aac547e2b41b1a
zaaack <zaaack@qq.com>
2017-10-23 22:39:32 +0800

    Dev

--- src/Fable.Validation.fs
+++ src/Fable.Validation.fs
@@ -1,11 +1,247 @@
-module Fable.Validation
+module Fable.Validation.Core
 
-open Fable.Core
+open System
+open System.Text.RegularExpressions
 
-(************************************************)
-(*     You should remove the next lines and     *)
-(*    start writing your library in this file   *)
-(************************************************)
+type ValidatorFlags = {
+    race: bool
+    skip: bool
+}
+type ValidatorInfo<'T, 'TError> =
+    | Input of (ValidatorFlags * 'T)
+    | Validator of (ValidatorFlags * 'T* Result<'T, 'TError list>)
+    | AsyncValidator of (ValidatorFlags * 'T * Async<Result<'T, 'TError list>>)
 
-[<Emit("$0 + $1")>]
-let add (x: int) (y: int) = jsNative
+type ValidationTester<'T, 'TError> =
+    | Test of ('T -> 'T option)
+    | AsyncTest of ('T -> Async<Result<'T option, 'TError>>)
+
+let inline internal castAny<'a, 'b> (a: 'a) = (a :> obj) :?> 'b
+
+let getInfoFlagsAndInput info =
+    match info with
+    | Input (race, input) -> (race, input)
+    | Validator (race, input, _) -> (race, input)
+    | AsyncValidator (race, input, _) -> (race, input)
+
+
+let ifInvalid<'T, 'TError> (test: ValidationTester<'T, 'TError>) (error: 'TError) (info: ValidatorInfo<'T, 'TError>): ValidatorInfo<'T, 'TError> =
+    let (flags, input) =  getInfoFlagsAndInput info
+    let {race = race; skip = skip} = flags
+    if skip then info
+    else
+        let inline mkResult input errors =
+            if List.isEmpty errors then Ok input
+            else Error errors
+        let inline mkResultWithLast lastResult input errors =
+            match lastResult with
+            | Ok input -> mkResult input errors
+            | Error errors' -> mkResult input (errors @ errors')
+
+
+        let inline asyncValidate () =
+            async {
+                let mutable newInput = input
+                let mutable testErrors = []
+                match test with
+                | Test test ->
+                    match test input with
+                    | None -> testErrors <- error::testErrors
+                    | Some input' -> newInput <- input'
+                | AsyncTest test ->
+                    let! result' = test input
+                    match result' with
+                    | Ok None -> testErrors <- error::testErrors
+                    | Ok(Some input') -> newInput <- input'
+                    | Error error' -> testErrors <- error'::testErrors
+
+                if race && List.isEmpty testErrors |> not then
+                    return mkResult newInput testErrors
+                else
+                    let mutable result = Ok newInput
+                    match info with
+                    | Input (race, input') ->
+                        failwithf "this should never happen for asyncValidator race:%A input:%A" race input'
+                    | Validator (_, input, result') ->
+                        result <- mkResultWithLast result' input testErrors
+                    | AsyncValidator (_, input, result') ->
+                        let! asyncInfoResult = result'
+                        result <- mkResultWithLast asyncInfoResult input testErrors
+                    return result
+            }
+
+        let mutable newInput = input
+        match test, info with
+        | AsyncTest _, _ | _, AsyncValidator _ ->
+            AsyncValidator (flags, input, asyncValidate ())
+        | Test test, Input (_, input) ->
+            let testErrors = 
+                match test input with
+                | Some input -> newInput <- input; []
+                | None -> [error]
+            Validator (flags, newInput, (mkResult input testErrors))
+        | Test test, Validator (_, input, result) ->
+            let testErrors = 
+                match test input with
+                | Some input -> newInput <- input; []
+                | None -> [error]
+            let result =
+                if race && List.isEmpty testErrors |> not then mkResult input testErrors
+                else mkResultWithLast result input testErrors
+            Validator (flags, input, result)
+
+let inline ifInvalidSync<'T, 'TError> (test: 'T -> 'T option) =
+    ifInvalid<'T, 'TError> (Test test)
+
+
+let inline ifInvalidAsync<'T, 'TError> (test: 'T -> Async<Result<'T option, 'TError>>) =
+    ifInvalid<'T, 'TError> (AsyncTest test)
+
+let baseValidateAsync<'T, 'TError, 'L when 'L : comparison>
+    raceField
+    (rules: 'T -> ('L * ValidatorInfo<obj, 'TError>) list)
+    input =
+        async {
+            let mutable msgMap = Map<'L, 'TError list> Seq.empty
+            let consumeResult key result =
+                match result with
+                | Ok _ -> ()
+                | Error errors ->
+                    msgMap <- Map.add key errors msgMap
+            let mutable tail = rules input
+            let mutable isBreak = false
+            while not isBreak && List.isEmpty tail |> not do
+                let (key, info) = tail.Head
+                tail <- tail.Tail
+                match info with
+                | Validator (_, _, result) ->
+                    result |> consumeResult key
+                | AsyncValidator (_, _, result) ->
+                    let! result = result
+                    consumeResult key result
+                | Input input' -> failwithf "Validation rules must be a function, but found input: %A" input'
+                if raceField && Map.isEmpty msgMap |> not then
+                    isBreak <- true
+
+            return
+                if Map.isEmpty msgMap then Ok input
+                else Error msgMap
+        }
+
+let inline allAsync<'T, 'TError, 'L when 'L : comparison> = baseValidateAsync<'T, 'TError, 'L> false
+let inline raceAsync<'T, 'TError, 'L when 'L : comparison> = baseValidateAsync<'T, 'TError, 'L> true
+
+// let internal createNewRecord (input: 'T) fields =
+//     let type' = typedefof<'T>
+//     FSharp.Reflection.FSharpValue.MakeRecord()
+
+let baseValidateSync<'T, 'TError, 'L when 'L : comparison>
+    raceField
+    (rules: 'T -> ('L * ValidatorInfo<obj, 'TError>) list)
+    input = 
+            let mutable msgMap = Map<'L, 'TError list> Seq.empty
+            let mutable tail = rules input
+            let mutable isBreak = false
+            while not isBreak && List.isEmpty tail |> not do
+                let (key, info) = tail.Head
+                tail <- tail.Tail
+                match info with
+                | Validator (_, _, result) ->
+                    match result with
+                    | Ok _ -> ()
+                    | Error errors ->
+                        (msgMap <- Map.add key errors msgMap)
+                | AsyncValidator (_, input', vali) ->
+                    failwithf "Sync validation cannot contain async rules: %A, whole input: %A" (input', vali) input
+                | Input input' -> failwithf "Validation rules must be a function, but found input: %A, whole input: %A" input' input
+                if raceField && Map.isEmpty msgMap |> not then
+                    isBreak <- true
+            if Map.isEmpty msgMap then Ok input
+            else Error msgMap
+
+let inline all<'T, 'TError, 'L when 'L : comparison> = baseValidateSync<'T, 'TError, 'L> false
+let inline race<'T, 'TError, 'L when 'L : comparison> = baseValidateSync<'T, 'TError, 'L> true
+
+let inline allSync<'T, 'TError, 'L when 'L : comparison> = all
+let inline raceSync<'T, 'TError, 'L when 'L : comparison> = race
+
+let inline test input = Input ({race=true;skip=false}, input)
+let inline testAll input = Input ({race=false;skip=false}, input)
+
+let inline endTest<'T, 'TError> = castAny<ValidatorInfo<'T, 'TError>, ValidatorInfo<obj, 'TError>>
+
+
+let skipNone<'T, 'TError> (info: ValidatorInfo<'T option, 'TError>): ValidatorInfo<'T, 'TError> =
+    let (flags, input) = getInfoFlagsAndInput info
+    let dummy = castAny<obj, 'T> null
+    match input with
+    | Some i -> Validator (flags, i, Ok(i))
+    | None -> Validator ({flags with skip = true}, dummy, Ok(dummy))
+
+
+let skipError<'T, 'TError, 'Error> (info: ValidatorInfo<Result<'T, 'TError>, 'Error>) : ValidatorInfo<'T, 'Error> =
+    let (flags, input) = getInfoFlagsAndInput info
+    let dummy = castAny<obj, 'T> null
+    match input with
+    | Ok i -> Validator (flags, i, Ok(i))
+    | Error _ -> Validator ({flags with skip = true}, dummy, Ok(dummy))
+
+let inline ifInvalidByBool<'T, 'TError>(tester: 'T -> bool) =
+    (fun input -> if tester input then Some input else None)  |> Test |> ifInvalid<'T, 'TError>
+
+let ifNone<'T, 'TError> error info =
+    let _ifNone = Option.isSome |> ifInvalidByBool
+    _ifNone error info |> skipNone<'T, 'TError>
+
+let ifError<'T, 'TError, 'Error> error info =
+    let inline isOk (input: Result<'T, 'TError>) =
+        match input with
+        | Ok _ -> true
+        | Error _ -> false
+    let _ifError = isOk |> ifInvalidByBool
+    _ifError error info |> skipError<'T, 'TError, 'Error>
+
+let ifBlank<'TError> = String.IsNullOrWhiteSpace |> ifInvalidByBool<string, 'TError>
+
+let ifBlankAfterTrim<'TError> =
+    (fun (input: string) -> 
+        let input = input.Trim()
+        if String.IsNullOrWhiteSpace input 
+        then None 
+        else Some input)
+    |> Test |> ifInvalid<string, 'TError>
+
+let ifNotGt min =
+    (fun input -> input > min) |> ifInvalidByBool
+
+let ifNotGte min =
+    (fun input -> input >= min) |> ifInvalidByBool
+
+let ifNotLt max =
+    (fun input -> input < max) |> ifInvalidByBool
+
+let ifNotLte max =
+    (fun input -> input <= max) |> ifInvalidByBool
+
+let ifNotMaxLen len =
+    (fun input -> Seq.length input <= len) |> ifInvalidByBool 
+
+let ifNotMinLen len =
+    (fun input -> Seq.length input >= len) |> ifInvalidByBool 
+
+module Regexs = 
+    let mail = Regex (@"^(([^<>()\[\]\\.,;:\s@""]+(\.[^<>()\[\]\\.,;:\s@""]+)*)|("".+""))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$", RegexOptions.Compiled ||| RegexOptions.ECMAScript)
+    let url = Regex (@"^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)$", RegexOptions.Compiled ||| RegexOptions.ECMAScript)
+
+
+
+let ifNotMail<'TError> = Regexs.mail.IsMatch |> ifInvalidByBool<string, 'TError>
+
+
+let ifNotUrl<'TError> = Regexs.url.IsMatch |> ifInvalidByBool<string, 'TError>
+
+#if FABLE_COMPILER 
+let ifNotDegist<'TError> = (String.forall(fun c -> c >= '0' && c <= '9')) |> ifInvalidByBool<string, 'TError>
+#else
+let ifNotDegist<'TError> = (String.forall(Char.IsDigit)) |> ifInvalidByBool<string, 'TError>
+#endif
--- tests/Fable.Validation.Tests.fs
+++ tests/Fable.Validation.Tests.fs
@@ -1,14 +1,127 @@
 module Fable.Validation.Tests
 
 open Fable.Core
-open Fable.Core.JsInterop
 open Fable.Validation
 open Fable.Core.Testing
+open Fable.Validation.Core
+open Fable.Import
 
 [<Global>]
 let it (msg: string) (f: unit->unit): unit = jsNative
 
-it "Adding works" <| fun () ->
-    let expected = 3
-    let actual = add 1 2
-    Assert.AreEqual(expected, actual)
+type Result<'T, 'TError> with
+    static member isOk (res: Result<'T, 'TError>) =
+        match res with
+        | Ok _ -> true
+        | Error _ -> false
+    static member isError (res: Result<'T, 'TError>) =
+        Result.isOk res |> not
+
+type People = {
+    name: string
+    age: int
+    phone: string
+    mail: string
+    url: string
+} 
+
+module People = 
+    let name = "name"
+    let age = "age"
+    let phone = "phone"
+    let mail = "mail"
+    let url = "url"
+
+
+let allRightPeople = {
+    name = "aaa"
+    age = 10
+    phone = "132721"
+    mail = "  aa@bb.com  "
+    url = "https://www.google.com"
+}
+
+it "Adding valid" <| fun () ->
+    let ret =
+        allRightPeople
+        |> (all (fun t ->
+                    [
+                        People.age, 
+                        testAll t.age 
+                            |> ifNotGt 0 "Age should greater then 0" 
+                            |> ifNotLt 20 "Age should less then 20" |> endTest
+                        People.name, testAll t.name |> ifBlank "Name shouldn't be empty"  |> endTest
+                        People.mail, testAll t.mail |> ifBlankAfterTrim "Mail shoudn't be empty" |> ifNotMail "Not valid mail" |> endTest
+                        People.phone, testAll t.phone |> ifNotDegist "Not numbers" |> endTest
+                        People.url, testAll t.url |> ifNotUrl "Not valid url" |> endTest
+                    ]))
+    ret |> Result.map (
+        fun input -> 
+            Assert.AreEqual(input.age, allRightPeople.age)
+            Assert.AreEqual(input.name, allRightPeople.name)
+            Assert.AreEqual(input.mail, allRightPeople.mail.Trim())
+       ) |> printfn "%A"
+    Assert.AreEqual(Result.isOk ret, true)
+
+
+it "Adding failed" <| fun () ->
+    let ageMsg = "Age should greater then 18"
+    let nameMsg = "Name should be degist"
+    let mailMsg = "Not valid Url"
+    let phoneMsg = "Not valid mail"
+    let urlMsg = "Not degist"
+    let ret =
+        allRightPeople
+        |> (all (fun t ->
+                    [
+                        People.age, testAll t.age |> ifNotGt 18 ageMsg |> endTest
+                        People.name, testAll t.name |> ifNotDegist nameMsg |> endTest
+                        People.mail, testAll t.mail |> ifNotUrl mailMsg |> endTest
+                        People.phone, testAll t.phone |> ifNotMail phoneMsg |> endTest
+                        People.url, testAll t.url |> ifNotDegist urlMsg |> endTest
+                    ]))
+    ret |> Result.mapError (
+        fun msgs -> 
+            Assert.AreEqual(msgs.[People.age].[0], ageMsg)
+            Assert.AreEqual(msgs.[People.name].[0], nameMsg)
+            Assert.AreEqual(msgs.[People.mail].[0], mailMsg)
+            Assert.AreEqual(msgs.[People.phone].[0], phoneMsg)
+            Assert.AreEqual(msgs.[People.url].[0], urlMsg)
+       ) |> printfn "%A"
+    Assert.AreEqual(Result.isError ret, true)
+// |> ifInvalidMail "Not valid mail sync" |> ifNotMinLen 12 "Name's minimal length is 12"
+
+// JS.console.time "sync"
+// let internal result =
+//     all (fun t ->
+//         [
+//             ("age", testAll t.age |> ifNotGt 18 "Age should greater then 18" |> endTest)
+//             ("name", testAll t.name |> ifBlank "Name shouldn't be empty" |> ifInvalidMail "Not valid mail sync" |> ifNotMinLen 12 "Name's minimal length is 12" |> endTest)
+//         ]) {name = ""; age = 15}
+// JS.console.timeEnd "sync"
+
+// printfn "all sync result: %A" result
+// printfn "mail: %A" (ifInvalidMail "Not valid mail" (Input ({race= false; skip= false}, "")))
+// let asyncResult =
+//     let rules t =
+//         [("age", testAll t.age |> ifNotGt 18 "Age should greater then 18" |> endTest);
+//         ("name", testAll t.name |> ifBlank "Name shouldn't be empty" |> ifInvalidMail "Not valid mail" |> ifNotMinLen 12 "Name's minimal length is 12" |> endTest)]
+//     async {
+//         JS.console.time "async"
+//         let! result' = allAsync rules {name = ""; age = 15}
+//         JS.console.timeEnd "async"
+//         printfn "all async result: %A" result'
+//     }
+//     |> Async.StartImmediate
+
+// let p = {name = ""; age = 15}
+// JS.console.time "raw"
+// let r' = String.IsNullOrWhiteSpace(p.name) || (Seq.length p.name) > 12
+// let r1' = p.age > 18
+// JS.console.timeEnd "raw"
+// // let asyncTest =
+// //     async {
+// //       return 1
+// //     } |> Async.RunSynchronously
+// // printfn "%A" asyncTest
+// // printfn "async result: %A" asyncResult
